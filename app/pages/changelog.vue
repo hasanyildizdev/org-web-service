a960d7a2109917086a04baaf4b268d678f5235dd
hasanyildizdev <hasanyildiz.97@hotmail.com>
2025-10-12 04:38:07 +0300

    changelog added

--- app/pages/changelog.vue
+++ app/pages/changelog.vue
@@ -5,17 +5,64 @@ useHead({
         { name: 'description', content: 'Ourganize Changelog - Latest news and updates' }
     ]
 })
+
+const versions = ref([
+  {
+    title: 'Ourganize 1.2',
+    description: 'Web app updated - with a new built-in component, better warnings, and performance improvements!',
+    image: '/assets/images/changelog/v1.2.png',
+    badge: 'v1.2',
+    date: '2025-09-12',
+    to: 'https://ourorganize.com/blog/v1-2',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  },
+  {
+    title: 'Ourganize 1.1',
+    description: 'Web app is out - we improved our web app!',
+    image: '/assets/images/changelog/v1.1.png',
+    badge: 'v1.1',
+    date: '2025-09-07',
+    to: 'https://ourorganize.com/blog/v1-1',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  },
+  {
+    title: 'Ourganize 1.0',
+    description: 'Ourganize 1.0.0 is out - we deployed our first version!',
+    image: '/assets/images/changelog/v1.0.png',
+    badge: 'v1.0',
+    date: '2025-09-01',
+    to: 'https://ourorganize.com/blog/v1-0',
+    target: '_blank',
+    ui: {
+      container: 'max-w-lg'
+    }
+  }
+])
 </script>
 
 <template>
     <UPage>
         <UPageSection class="max-w-4xl mx-auto">
             <UPageHeader 
-                :headline="$t('Changelog')"
-                :title="$t('Stay tuned for the latest news, updates, and insights about our platform.')"
-                :description="$t('Changelog coming soon...')"
+                :headline="$t('In Sync')"
+                :title="$t('Changelog')"
+                :description="$t('We are working hard to improve our platform and bring you the best experience possible.')"
                 :ui="{ root: 'pb-0 border-none' }"
             />
+            <UChangelogVersions>
+                <UChangelogVersion
+                    v-for="(version, index) in versions"
+                    :key="index"
+                    v-bind="version"
+                    :to="version.to"
+                    />
+            </UChangelogVersions>
         </UPageSection>
     </UPage>
 </template>
