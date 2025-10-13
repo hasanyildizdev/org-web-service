9be72a24e0294535f7cbbee300257df2b37786d5
hasanyildizdev <hasanyildiz.97@hotmail.com>
2025-10-13 03:32:04 +0300

    footer responsive update

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,23 +1,21 @@
-<script setup lang="ts">
-</script>
 <template>
     <UFooter class="mt-4 border-t border-gray-200 dark:border-gray-800">
         <template #left>
-            <div class="flex flex-col items-start mb-4 md:mb-0">
-                <span class="text-lg font-semibold text-primary dark:text-white">Ourganize</span>
-                <span class="text-sm text-gray-500 dark:text-gray-400">{{ $t('Where time works for you.') }}</span>
-            </div>
-        </template>
-        <template #right>
-            <div class="text-center md:text-right">
+            <div class="w-full sm:w-auto">
                 <p class="text-muted text-sm">
                     Copyright © {{ new Date().getFullYear() }} {{ $t('All rights reserved.') }}
                 </p>
-                <div class="mt-2 flex justify-center md:justify-end space-x-3">
+                <div class="w-full mt-2 space-x-3">
                     <NuxtLink to="/privacy-policy" class="text-xs text-gray-500 hover:text-primary transition-colors duration-300">{{ $t('Privacy Policy') }}</NuxtLink>
                     <NuxtLink to="/terms-of-service" class="text-xs text-gray-500 hover:text-primary transition-colors duration-300">{{ $t('Terms of Service') }}</NuxtLink>
                     <NuxtLink to="/cookies" class="text-xs text-gray-500 hover:text-primary transition-colors duration-300">{{ $t('Cookies') }}</NuxtLink>
                 </div>
+            </div>
+        </template>
+        <template #right>
+            <div class="w-full sm:w-auto flex flex-col items-start mb-4 md:mb-0">
+                <span class="text-lg font-semibold text-secondary dark:text-white">Ourganize</span>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{ $t('Where time works for you.') }}</span>
             </div>
         </template>
     </UFooter>
